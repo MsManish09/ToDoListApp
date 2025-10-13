8c28d061f165f9d7df01df8295ac408e3e6bcda9
MsManish09 <msmanish680@gmail.com>
2025-10-14 02:40:42 +0530

    Header component is initaized and completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header(){
+    return(
+        <div className=" flex justify-center items-center p-2 bg-gradient-to-r from-blue-500 to-purple-600 h-[85px]  " >
+            <img src="src/utils/TaskTickLogo.png" alt="TaskTick site logo" className=" h-[70px] " />
+        </div>
+    )
+}
+
+export default Header
+
