9bc9782d047dd2a88494a8530e0bd11549172a9d
MsManish09 <msmanish680@gmail.com>
2025-10-15 04:27:39 +0530

    basic add task functionality done, logic to store the task info is to be done next

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,56 @@
+import { useState } from "react";
 import ToDoTask from "./ToDoTask";
 
 function ToDoList(){
 
+    const [taskName, setTaskName] = useState('')
+    const [deadline, setDeadline] = useState()
+
+    // const tasksArray = []
+    // const [tasksList, setTasksList] = useState(tasksArray)
+
+    // const task = {
+    //     name: '',
+    //     deadline: null
+    // }
+
+    function handleTaskName(e){
+        setTaskName(e.target.value)
+        console.log(e.target.value)
+    }
+
+    function handleDeadlineChange(e){
+        setDeadline(e.target.value)
+        console.log(e.target.value)
+    }
+
+    function handleAddTask(){
+        // task.name = taskName
+        // task.deadline = deadline
+
+        // tasksArray.push(task)
+
+        // setTasksList(tasksArray)
+    }
+
     return(
         <div  className=" font-mono  " >
 
-            <div>
-                <h1 className=" text-center text-3xl mt-4 font-bold " >My Tasks</h1>
-                <p className=" text-center mt-2 " >Tick off your tasks, one at a time✔️</p>
+            <div className=" flex flex-col gap-4 justify-center items-center p-2 " >
+                <h1 className=" text-center text-3xl  font-bold " >My Tasks</h1>
+                <p className=" text-center  " >Tick off your tasks, one at a time✔️</p>
+
+                <div id="addTask">
+                    <form action="" className= " flex justify-center items-center gap-3 border-2 border-solid border-purple-500 p-2 pl-4 pr-4  bg-gradient-to-r from-purple-300 to-blue-300 flex-wrap " >
+                        <input type="text" id='taskName' required placeholder='Add new task' className=' bg-blue-200 rounded-2xl p-2 pl-4 ' value={taskName} onChange={handleTaskName} />
+                        <div className=" flex flex-col justify-center items-center " >
+                            <p className=" font-bold " >Deadline</p>
+                            <input type="date" id='Deadline' required onChange={handleDeadlineChange} />
+                        </div>
+                        
+                        <h1 id='addTaskBtn' className=" bg-blue-500 rounded-2xl p-2 font-bold hover:scale-95 w-[50px] text-center " onClick={handleAddTask} >Add</h1>
+                    </form>
+                </div>
             </div>
 
             <div className=" flex gap-4 justify-center mt-6 flex-wrap "  >
